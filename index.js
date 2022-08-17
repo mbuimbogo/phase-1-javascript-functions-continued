c3c10f616a204217c5fd80b761f7b6c846b9ead8
“mbuimbogo” <peter.mbogo@student.moringaschool.com>
2022-08-17 16:34:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+    };
+    saturdayFun("roller-skate");
+    saturdayFun("bathe my dog")
+
+    const mondayWork = function (work= "go to the office") {
+    return `This Monday, I will ${work}.`;
+    }
+    mondayWork();
+
+   function wrapAdjective(adject){
+    return function (inner){
+        return `You are ${adject}${inner}${adject}!`;
+    }
+   }
+   wrapAdjective('*')('a hard worker')
+   wrapAdjective('||') ('a dedicated programmer')
